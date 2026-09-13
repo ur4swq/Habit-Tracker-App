f36914d877934249f7b1bda9dc426b2f9669bd68
ur4swq <urasy308@gmail.com>
2026-09-13 12:59:58 +0300

    feat: add Electron desktop app and Windows build support

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+app.setAppUserModelId("com.ur4swq.habittracker");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 900,
+    minHeight: 650,
+    autoHideMenuBar: true,
+
+    icon: path.join(__dirname, "assets", "icon.png"),
+
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
